a6abad014229f0fafb19e13062a317d2441e0c07
marcaumdesign <navalservice1@gmail.com>
2025-05-21 20:07:48 -0300

    📦 UPDATE: refatorar Layout removendo dependências de contexto de cursor
    - Removido o uso do CursorColorContext e a implementação do SmoothCursor para simplificar o layout.
    - Ajustada a estrutura do componente Layout para manter a funcionalidade sem as dependências removidas.

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ import {
   Outlet,
 } from '@remix-run/react';
 import { useState } from "react";
-import { CursorColorContext } from "~/components/shad-cn/ui/CursorContext";
 
 import resetStyles from '~/styles/reset.css?url';
 // Supports weights 100-900
@@ -18,14 +17,10 @@ import appStyles from '~/styles/app.css?url';
 import tailwindCss from '~/styles/tailwind.css?url';
 import {PageLayout} from '~/components/PageLayout';
 import {RootLoader} from './root';
-import { SmoothCursor } from "~/components/shad-cn/ui/smooth-cursor";
-
 
 export default function Layout() {
   const nonce = useNonce();
   const data = useRouteLoaderData<RootLoader>('root');
-  const [cursorColor, setCursorColor] = useState("black");
-  const [borderColor, setBorderColor] = useState("#303172");
 
   return (
     <html lang="en">
@@ -39,24 +34,21 @@ export default function Layout() {
         <Links />
       </head>
       <body className="relative">
-        <CursorColorContext.Provider value={{ color: cursorColor, setColor: setCursorColor, borderColor, setBorderColor }}>
-          <SmoothCursor color={cursorColor} size={24} borderColor={borderColor} />
-          {data ? (
-            <Analytics.Provider
-              cart={data.cart}
-              shop={data.shop}
-              consent={data.consent}
-            >
-              <PageLayout {...data}>
-                <Outlet />
-              </PageLayout>
-            </Analytics.Provider>
-          ) : (
-            <Outlet />
-          )}
-          <ScrollRestoration nonce={nonce} />
-          <Scripts nonce={nonce} />
-        </CursorColorContext.Provider>
+        {data ? (
+          <Analytics.Provider
+            cart={data.cart}
+            shop={data.shop}
+            consent={data.consent}
+          >
+            <PageLayout {...data}>
+              <Outlet />
+            </PageLayout>
+          </Analytics.Provider>
+        ) : (
+          <Outlet />
+        )}
+        <ScrollRestoration nonce={nonce} />
+        <Scripts nonce={nonce} />
       </body>
     </html>
   );

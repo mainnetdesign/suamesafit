762852db76bc9fbbe42a48d6e717aac4da16535d
marcaumdesign <navalservice1@gmail.com>
2025-07-24 12:50:21 -0300

    📦 UPDATE: atualização dos depoimentos no arquivo fallback-testimonials.ts
    - Alterados os textos e autores dos depoimentos para refletir novas recomendações e experiências.
    - Garantida a consistência nas informações apresentadas, destacando a qualidade das marmitas.

--- app/data/fallback-testimonials.ts
+++ app/data/fallback-testimonials.ts
@@ -8,22 +8,22 @@ export const FALLBACK_TESTIMONIALS: TestimonialData[] = [
   {
     id: '1',
     rating: 5,
-    text: 'Eu e meu marido trabalhamos o dia todo e, com as marmitas, ganhamos tempo e continuamos comendo bem. Não trocamos por nada!',
-    authorName: 'Daniela K. e João V.',
+    text: 'Eu e meu marido trabalhamos o dia todo e somos nutricionistas, com as marmitas, ganhamos tempo e continuamos comendo bem. Não trocamos por nada!',
+    authorName: 'Christian e Evelin',
     image: testimonial1,
   },
   {
     id: '2',
     rating: 5,
-    text: 'Vivia de iFood e não tinha como fazer uma dieta saudável. Agora com as marmitas da Erike e Jana, posso comer bem e continuar com a dieta de forma fácil!',
-    authorName: 'Marcos S.',
+    text: 'Eu como nutricionista sempre recomendo pros meus pacientes a comer as marmitas da Erike e Jana. São deliciosas e saudáveis!',
+    authorName: 'Rafael Polonio',
     image: testimonial2,
   },
   {
     id: '3',
     rating: 5,
-    text: 'Eu como nutricionista sempre recomendo pros meus pacientes a comer as marmitas da Erike e Jana. São deliciosas e saudáveis!',
-    authorName: 'João M.',
+    text: 'Vivia de iFood e não tinha como fazer uma dieta saudável. Agora com as marmitas da Erike e Jana, posso comer bem e continuar com a dieta de forma fácil!',
+    authorName: 'Rafael Reis',
     image: testimonial3,
   },
 ]; 
